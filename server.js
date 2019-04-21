fd4d4c6f15d13135f64a9f3f76883cf236886b32
toshyss <toshy.ss@gmail.com>
2019-04-21 22:54:34 +0900

    added package.json and auto build

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const user = process.env.USER;
+const pass = process.env.PASS;
+
+app.set('port', process.env.PORT || 3000);
+
+if (user && pass) {
+  app.use(express.basicAuth(user, pass));
+}
+
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Server listening on port %s', app.get('port'));
+});
